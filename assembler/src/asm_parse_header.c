bdd988142e626d8d7afd626cc813634c05ed67ec
jacksonwb <jbeall@e1z3r5p25.42.us.org>
2019-04-25 10:51:10 -0700

    split to files, added line error output

---
+++ assembler/src/asm_parse_header.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   asm_parse_header.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbeall <jbeall@student.42.us.org>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/25 10:41:59 by jbeall            #+#    #+#             */
+/*   Updated: 2019/04/25 10:47:41 by jbeall           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "asm.h"
+
+int capture_header_field(char *in, t_asm *out, char *field, size_t len)
+{
+	char *val;
+	in = skip_space(in);
+	if (!ft_strncmp(in, field, ft_strlen(field)))
+	{
+		in += ft_strlen(field);
+		in = skip_space(in);
+		if (*in == '"')
+		{
+			if(!(val = capture_to_quote(++in)))
+				asm_error("syntax error", "no end quote", out->line);
+			if (ft_strlen(val) > len)
+				ft_exit("error: val exceeds length", 1);
+			if (!ft_strcmp(field, NAME_CMD_STRING))
+				ft_strlcat(out->header->prog_name, val, len);
+			else if (!ft_strcmp(field, COMMENT_CMD_STRING))
+				ft_strlcat(out->header->comment, val, len);
+		}
+		else
+			asm_error("sytax error", "bad character", out->line);
+	}
+	else
+		return (0);
+	return (1);
+}
+
+int contains_non_comment(char *st)
+{
+	st = skip_space(st);
+	if (*st && *st != '#')
+		return (1);
+	return (0);
+}
+
+int valid_header_block(t_uvector *block, t_asm *out)
+{
+	int quote;
+	unsigned i;
+
+	quote = 0;
+	i = 0;
+	while (i < block->length && quote < 2)
+	{
+		if (block->data[i] == '"')
+			quote++;
+		i++;
+	}
+	if (quote == 2 && contains_non_comment((char*)(block->data + i)))
+		asm_error("syntax error", "bad character", out->line);
+	if (quote == 2)
+		return (1);
+	return (0);
+}
+
+char *capture_to_space(char *in)
+{
+	int len;
+	static char buf[CAPTURE_BUFF];
+
+	len = 0;
+	ft_bzero(buf, CAPTURE_BUFF);
+	while (in[len] && len < CAPTURE_BUFF - 1 && !ft_isspace(in[len]))
+		len++;
+	ft_strlcat(buf, in, CAPTURE_BUFF);
+	return buf;
+}
+
+char *capture_to_quote(char *in)
+{
+	int len;
+	static char buf[CAPTURE_BUFF];
+
+	len = 0;
+	ft_bzero(buf, CAPTURE_BUFF);
+	while (in[len] && len < CAPTURE_BUFF - 1 && in[len] != '"')
+		len++;
+	ft_strlcat(buf, in, len + 1);
+	return buf;
+}
--- assembler/src/main.c
+++ assembler/src/main.c
@@ -6,114 +6,16 @@
 /*   By: jbeall <jbeall@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/22 18:28:10 by lkaser            #+#    #+#             */
-/*   Updated: 2019/04/24 21:27:47 by jbeall           ###   ########.fr       */
+/*   Updated: 2019/04/25 10:48:59 by jbeall           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
-int	ft_isspace(int c)
+void asm_error(char *er_name, char *er_type, int line)
 {
-	if (c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r'
-		|| c == ' ')
-		return (1);
-	else
-		return (0);
-}
-
-char *skip_space(char *in)
-{
-	while (ft_isspace(*in))
-		in++;
-	return (in);
-}
-
-char *skip_comment(char *in)
-{
-	while (*in != '\n')
-		in++;
-	return in++;
-}
-
-char *capture_to_space(char *in)
-{
-	int len;
-	static char buf[CAPTURE_BUFF];
-
-	len = 0;
-	ft_bzero(buf, CAPTURE_BUFF);
-	while (in[len] && len < CAPTURE_BUFF - 1 && !ft_isspace(in[len]))
-		len++;
-	ft_strlcat(buf, in, CAPTURE_BUFF);
-	return buf;
-}
-
-char *capture_to_quote(char *in)
-{
-	int len;
-	static char buf[CAPTURE_BUFF];
-
-	len = 0;
-	ft_bzero(buf, CAPTURE_BUFF);
-	while (in[len] && len < CAPTURE_BUFF - 1 && in[len] != '"')
-		len++;
-	ft_strlcat(buf, in, len + 1);
-	return buf;
-}
-
-int capture_header_field(char *in, t_header *header, char *field, size_t len)
-{
-	char *val;
-	in = skip_space(in);
-	if (!ft_strncmp(in, field, ft_strlen(field)))
-	{
-		in += ft_strlen(field);
-		in = skip_space(in);
-		if (*in == '"')
-		{
-			if(!(val = capture_to_quote(++in)))
-				ft_exit("syntax error: no end quote", 1);
-			if (ft_strlen(val) > len)
-				ft_exit("error: val exceeds length", 1);
-			if (!ft_strcmp(field, NAME_CMD_STRING))
-				ft_strlcat(header->prog_name, val, len);
-			else if (!ft_strcmp(field, COMMENT_CMD_STRING))
-				ft_strlcat(header->comment, val, len);
-		}
-		else
-			ft_exit("sytax error: bad character", 2);
-	}
-	else
-		return (0);
-	return (1);
-}
-
-int contains_non_comment(char *st)
-{
-	st = skip_space(st);
-	if (*st && *st != '#')
-		return (1);
-	return (0);
-}
-
-int valid_header_block(t_uvector *block)
-{
-	int quote;
-	unsigned i;
-
-	quote = 0;
-	i = 0;
-	while (i < block->length && quote < 2)
-	{
-		if (block->data[i] == '"')
-			quote++;
-		i++;
-	}
-	if (quote == 2 && contains_non_comment((char*)(block->data + i)))
-		ft_exit("syntax error: bad character", 1);
-	if (quote == 2)
-		return (1);
-	return (0);
+	ft_printf("%s: %s [line %d]\n", er_name, er_type, line);
+	exit(0);
 }
 
 void ft_uvector_reset(t_uvector *vector, int size)
@@ -137,33 +39,22 @@ int parse_header(int fd, t_asm *out)
 	in.data[0] = '\0';
 	while (!(name_flag && comment_flag))
 	{
-		while (get_next_line(fd, &buf))
+		while (asm_readline(out, fd, &buf))
 		{
 			ft_string_append(&in, buf);
 			ft_strdel(&buf);
-			if (valid_header_block(&in))
+			if (valid_header_block(&in, out))
 				break;
 			ft_string_append(&in, "\n");
 		}
-		if(!name_flag && (name_flag = capture_header_field((char*)in.data, out->header, NAME_CMD_STRING, PROG_NAME_LENGTH)))
+		if(!name_flag && (name_flag = capture_header_field((char*)in.data, out, NAME_CMD_STRING, PROG_NAME_LENGTH)))
 			ft_uvector_reset(&in, 1);
-		else if (!comment_flag && (comment_flag = capture_header_field((char*)in.data, out->header, COMMENT_CMD_STRING, COMMENT_LENGTH)))
+		else if (!comment_flag && (comment_flag = capture_header_field((char*)in.data, out, COMMENT_CMD_STRING, COMMENT_LENGTH)))
 			ft_uvector_reset(&in, 1);
 		else
-			ft_exit("error: incomplete header", 1);
+			asm_error("error", "incomplete header", out->line);
 	}
 	return (0);
-}
-
-unsigned reverse_endian(unsigned in)
-{
-	unsigned out;
-
-	out = in >> 3 * 8;
-	out += (in & (0xff << 2 * 8)) >> 1 * 8;
-	out += (in & (0xff << 1 * 8)) << 1 * 8;
-	out += (in & (0xff << 0 * 8)) << 3 * 8;
-	return (out);
 }
 
 void parse(int fd, t_asm *out)
