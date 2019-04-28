62bff1ddad995e86da96d30e1e9bda831b32d63a
tcheee <B00705488@essec.edu>
2019-04-27 21:45:53 -0700

    test body parser

--- assembler/test_champs/op_test.s
+++ assembler/test_champs/op_test.s
@@ -1,11 +1,11 @@
-  .name "op_test"
+  .name  "op_test"
 
-.comment "op testing file"
+.comment "op testing file" #""
 
 #comment
 
 label: #comment
-live: live %01
+live: live %013543543543543545454
 		ld: ld 01, r2
-		ld : live,r1
+		ld :live,r1
 		fork %1
