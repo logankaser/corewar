f74b3ff0a90704dc1e874aea22892dc79657d802
tcheee <B00705488@essec.edu>
2019-05-03 16:14:30 -0700

    norm

--- virtual_machine/src/op/aff.c
+++ virtual_machine/src/op/aff.c
@@ -6,7 +6,7 @@
 /*   By: ztisnes <ztisnes@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/01 17:46:04 by ztisnes           #+#    #+#             */
-/*   Updated: 2019/05/03 15:30:02 by tcherret         ###   ########.fr       */
+/*   Updated: 2019/05/03 16:13:50 by tcherret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ void	aff(t_vm *vm, t_process *p, t_instruction_meta *im)
 {
 	int 			p1;
 	char 			ascii;
-
 
 	p1 = param_load(im, vm->arena, p->pc, 0);
 	if (VALID_REG(p1))
