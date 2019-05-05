c64a89eac13ecc87d4a593f8e097bc7f123e8008
zedin27 <ztisneshamzeh@csumb.edu>
2019-05-05 15:40:05 -0700

    added operations in the intruction table

--- virtual_machine/src/instruction_dispatch.c
+++ virtual_machine/src/instruction_dispatch.c
@@ -6,7 +6,7 @@
 /*   By: lkaser <lkaser@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/26 14:04:38 by lkaser            #+#    #+#             */
-/*   Updated: 2019/04/26 14:04:40 by lkaser           ###   ########.fr       */
+/*   Updated: 2019/05/05 15:38:29 by ztisnes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,9 @@ t_instruction	*const g_instruction_dispatch[16] = {
 	st,
 	no_op,
 	no_op,
-	no_op,
-	no_op,
-	no_op,
+	and,
+	or,
+	xor,
 	no_op,
 	ldi,
 	sti,
