d7e1c97c549497cad6af66638e2ee350fd66a008
zedin27 <ztisneshamzeh@csumb.edu>
2019-05-05 15:24:49 -0700

    fixed tiny bug in the reg number, operations and or xor

--- virtual_machine/src/op/and_or_xor.c
+++ virtual_machine/src/op/and_or_xor.c
@@ -6,7 +6,7 @@
 /*   By: ztisnes <ztisnes@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/30 17:02:59 by ztisnes           #+#    #+#             */
-/*   Updated: 2019/05/04 17:50:03 by ztisnes          ###   ########.fr       */
+/*   Updated: 2019/05/05 15:17:07 by ztisnes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,35 +16,35 @@ void	and(t_vm *vm, t_process *p, t_decode *d)
 {
 	int32_t	p1;
 	int32_t	p2;
+	int32_t	p3;
 
 	p1 = param_read(d, vm->arena, p, 0);
 	p2 = param_read(d, vm->arena, p, 1);
-	if (d->values[2] == REG)
-		p->registers[d->values[2] - 1] = p1 & p2;
+	p3 = d->values[2];
+	p->registers[p3 - 1] = p1 & p2;
 	p->carry = p->registers[p3 - 1] ? 0 : 1;
-	ft_printf("Good?");
 }
 void	or(t_vm *vm, t_process *p, t_decode *d)
 {
 	int32_t p1;
 	int32_t p2;
+	int32_t	p3;
 
 	p1 = param_read(d, vm->arena, p, 0);
 	p2 = param_read(d, vm->arena, p, 1);
-	if (d->values[2] == REG)
-		p->registers[d->values[2] - 1] = p1 | p2;
+	p3 = d->values[2];
+	p->registers[p3 - 1] = p1 | p2;
 	p->carry = p->registers[p3 - 1] ? 0 : 1;
-	ft_printf("Good?");
 }
 void	xor(t_vm *vm, t_process *p, t_decode *d)
 {
 	int32_t p1;
 	int32_t p2;
+	int32_t	p3;
 
 	p1 = param_read(d, vm->arena, p, 0);
 	p2 = param_read(d, vm->arena, p, 1);
-	if (d->values[2] == REG)
-		p->registers[d->values[2] - 1] = p1 ^ p2;
+	p3 = d->values[2];
+	p->registers[p3 - 1] = p1 ^ p2;
 	p->carry = p->registers[p3 - 1] ? 0 : 1;
-	ft_printf("Good?");
 }
--- virtual_machine/test.s
+++ virtual_machine/test.s
@@ -2,6 +2,10 @@
 
 .comment "dead alive"
 
-st r1,6
+and 1,1,r1
+or 2,4,r2
+xor 2,2,r3
+xor 2,%10,r16
 
-live %42
+
+; live %42
