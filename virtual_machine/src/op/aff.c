db11ec02b11e00d602965b71deed0bd08ddaa378
tcheee <B00705488@essec.edu>
2019-05-06 15:14:13 -0700

    aff modify with the new structure

--- virtual_machine/src/op/aff.c
+++ virtual_machine/src/op/aff.c
@@ -6,7 +6,7 @@
 /*   By: ztisnes <ztisnes@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/01 17:46:04 by ztisnes           #+#    #+#             */
-/*   Updated: 2019/05/04 13:47:39 by tcherret         ###   ########.fr       */
+/*   Updated: 2019/05/06 15:13:37 by tcherret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,10 @@ void	aff(t_vm *vm, t_process *p, t_decode *d)
 	int 			p1;
 	char 			ascii;
 
-	p1 = param_read(d, vm->arena, p, 0);
-	ascii = p->registers[p1 - 1] % 256;
-	write(1, &ascii, 1);
+	if (!vm->quiet)
+	{
+		p1 = param_read(d, vm->arena, p, 0);
+		ascii = p->registers[p1 - 1] % 256;
+		write(1, &ascii, 1);
+	}
 }
